60db32bd85cb19ef433037cf46e089595b4b55a2
Danial <imdaniald@gmail.com>
2022-11-23 18:38:04 +0330

    move to src

--- src/server.ts
+++ src/server.ts
@@ -1,9 +1,11 @@
 import fs from "fs";
+import path from "path";
 import { DrawIoConvertor } from "./draw-io-convertor";
 import { drawIoNormalize } from "./draw-io-normilize";
 import { IOriginElement } from "./types";
 
-const data = JSON.parse(fs.readFileSync("data.json", "utf-8"));
+const data = JSON.parse(fs.readFileSync("./src/data.json", "utf-8"));
+
 const elements = data.elements.map((e: IOriginElement) => drawIoNormalize(e));
 const c = new DrawIoConvertor(elements);
 const result = c.convert();
